9363007627686199469bc23ec4c49beb9f2e2390
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-04-27 00:39:25 -0600

    Created Post model

---
+++ ProductHunt/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  ProductHunt
+//
+//  Created by Student Laptop_7/19_1 on 4/27/21.
+//
+
+// A product retreived from the ProductHunt API
+struct Post {
+   // Various properties of a post that we either need or want to display
+   let id: Int
+   let name: String
+   let tagline: String
+   let votesCount: Int
+   let commentsCount: Int
+}
