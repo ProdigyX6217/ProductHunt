26e905380c06c26c9fb620a7e530d40d08bdd9f9
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-05-03 21:53:08 -0600

    Built Comments view and controller

---
+++ ProductHunt/CommentTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentTableViewCell.swift
+//  ProductHunt
+//
+//  Created by Student Laptop_7/19_1 on 5/3/21.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var commentTextView: UITextView!
+    
+// Below functions are not needed, they were auto-generated
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    
+}
