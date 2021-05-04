eb145175e6ec43fe60b1cef53ee579cec3fca5d4
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-05-04 06:25:42 -0600

    Completed PH Reader

--- ProductHunt/CommentsViewController.swift
+++ ProductHunt/CommentsViewController.swift
@@ -11,40 +11,55 @@ class CommentsViewController: UIViewController {
 
     @IBOutlet weak var commentsTableView: UITableView!
     
-    var comments: [String]! = [] {
+    var comments: [Comment]! = [] {
        didSet {
-            // TODO: fill in later when we have real data
+            commentsTableView.reloadData()
        }
     }
+    
+    var postID: Int!
+    var networkManager = NetworkManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         commentsTableView.dataSource = self
         commentsTableView.delegate = self
+        updateComments()
     }
     
+    func updateComments() {
+             networkManager.getComments(postID) { result in
+                 switch result {
+                 case let .success(comments):
+                     self.comments = comments
+                 case let .failure(error):
+                     print(error)
+                 }
+             }
+         }
 }
 
 // All methods here operate the same way we did for Posts
 
 // MARK: UITableViewDatasource
-extension CommentsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return comments.count
-    }
+ extension CommentsViewController: UITableViewDataSource {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return comments.count
+     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath) as! CommentTableViewCell
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell", for: indexPath) as! CommentTableViewCell
 
-        let comment = comments[indexPath.row]
-        cell.commentTextView.text = comment
-        return cell
-    }
-}
+         let comment = comments[indexPath.row]
+         cell.commentTextView.text = comment.body
+         return cell
+     }
+ }
 
-// MARK: UITableViewDelegate
+ // MARK: UITableViewDelegate
 extension CommentsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 125
     }
 }
+
--- ProductHunt/FeedViewController.swift
+++ ProductHunt/FeedViewController.swift
@@ -14,68 +14,65 @@ class FeedViewController: UIViewController {
     // Array of Post objects to simulate real data coming in
     // Make sure each property that we're assigning to a UI element has a value for each mock Post.
     var posts: [Post] = [] {
-       didSet {
-           feedTableView.reloadData()
-       }
-    }
-    
-    var networkManager = NetworkManager()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        feedTableView.dataSource = self
-        feedTableView.delegate = self
-        updateFeed()
-    }
-    
-    func updateFeed() {
-      // call our network manager's getPosts method to update our feed with posts
-       networkManager.getPosts() { result in
-           self.posts = result
-       }
-    }
-    
-}
-
-// MARK: UITableViewDataSource
-extension FeedViewController: UITableViewDataSource {
-    /// Determines how many cells will be shown on the table view.
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     // return the actual number of posts we receive
-     return posts.count
-    }
-
-    /// Creates and configures each cell.
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
-
-     // retrieve from the actual posts, and not mock data
-     let post = posts[indexPath.row]
-     cell.post = post
-     return cell
-    }
-}
-
-
-// MARK: UITableViewDelegate
-extension FeedViewController: UITableViewDelegate {
-    // Code to handle cell events goes here...
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-      // provide a fixed size
-      return 250
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post = posts[indexPath.row]
-        // Get the storyboard
-         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        // Get the commentsView from the storyboard
-         guard let commentsView = storyboard.instantiateViewController(withIdentifier: "commentsView") as? CommentsViewController else {
-           return
-         }
-         // add mock comments
-         commentsView.comments = ["Blah blah blah!", "Good app.", "Wow."]
-         navigationController?.pushViewController(commentsView, animated: true)
+           didSet {
+               feedTableView.reloadData()
+           }
         }
         
-}
+        var networkManager = NetworkManager()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+            feedTableView.dataSource = self
+            feedTableView.delegate = self
+            updateFeed()
+         }
+
+         func updateFeed() {
+            networkManager.getPosts() { result in
+                switch result {
+                case let .success(posts):
+                  self.posts = posts
+                case let .failure(error):
+                  print(error)
+                }
+            }
+         }
+     }
+
+     // MARK: UITableViewDataSource
+    extension FeedViewController: UITableViewDataSource {
+       // Determines how many cells will be shown on the table view.
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the actual number of posts we receive
+        return posts.count
+       }
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+        // retrieve from the actual posts, and not mock data
+        let post = posts[indexPath.row]
+        cell.post = post
+        return cell
+       }
+    }
+    // MARK: UITableViewDelegate
+    extension FeedViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            // provide a fixed size
+        return 250
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let post = posts[indexPath.row]
+
+             // Get the storyboard
+              let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+              guard let commentsView = storyboard.instantiateViewController(withIdentifier: "commentsView") as? CommentsViewController else {
+                return
+              }
+              // set the post id for the comments
+              commentsView.postID = post.id
+              navigationController?.pushViewController(commentsView, animated: true)
+         }
+     }
